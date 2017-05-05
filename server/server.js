6c520678f3e5581f3edd96c922d511f3b60157db
Kevin Nguyen <knguy47@gmail.com>
2017-05-05 14:05:13 -0500

    Current Working Version

--- server/server.js
+++ server/server.js
@@ -1,21 +1,21 @@
 var path = require('path');
 var express = require('express');
 
-
-
 var app = express();
 
-
-
 // set path for static files
-app.use(express.static(path.join(__dirname, '/www')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', function(req, res) {
   res.send('Hello, humans!');
 
 });
 
-var port = 3003;
+app.get('/users', function(req, res) {
+  res.send('hi')
+});
+
+var port = process.env.PORT || 3003;
 
 app.listen(port, () => {
   console.log(`server listening at ${port}`)
