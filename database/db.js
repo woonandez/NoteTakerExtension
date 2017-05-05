1bc3ccc584dc968b13c68a51e2b74140d20f5135
David Hsiao <dhsiao89@gmail.com>
2017-05-05 10:31:48 -0700

    local db set up

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-mongoose.connect('mongodb://127.0.0.1:27017/NoteExtension')
+mongoose.connect('mongodb://127.0.0.1:27017/NoteExtension');
 mongoose.Promise = global.Promise;
 
 var db = mongoose.connection;
