4c5fcb5d7a18ed2a32c7f297ebf9e319c6e3c9d0
Kevin Nguyen <knguy47@gmail.com>
2017-05-08 17:34:25 -0500

    Add Header to Server

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ var app = express();
 app.use(bodyParser.json());
 // set path for static files
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "OPTIONS, DELETE, POST, GET, UPDATE");
+  next();
+});
 
 app.get('/', function(req, res) {
   res.send('Hello, humans!');
@@ -28,7 +34,7 @@ app.get('/api/users', function(req, res) {
 app.post('/api/users', (req, res) => {
   User.addUser(req.body.name, req.body.password, req.body.uri, req.body.notes);
 
-  res.send(201).send('Post Success');
+  res.status(201).send('Post Success');
 });
 
 var port = process.env.PORT || 3003;
