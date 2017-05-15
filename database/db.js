d44724bff13a288a7ca80c9045cdef4a55edaa2d
Kevin Nguyen <knguy47@gmail.com>
2017-05-15 16:00:26 -0500

    Heroku

--- database/db.js
+++ database/db.js
@@ -5,7 +5,7 @@ var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 
                 replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
 
 
-var MONGODB_URI = 'mongodb://localhost:27017/NoteExtension';
+var MONGODB_URI = 'mongodb://heroku_sz03msp8:f8dr4mvag1ke4iuaabbc4hna17@ds133221.mlab.com:33221/heroku_sz03msp8';
 mongoose.connect(MONGODB_URI, options);
 
 mongoose.Promise = global.Promise;
