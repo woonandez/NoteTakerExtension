ff3fa300fa0e0d72fd5e3f43416ceabb34a4091c
woonandez <matthew.allen.fernandez@gmail.com>
2017-05-19 21:31:37 -0700

    closes #41

--- src/info.js
+++ src/info.js
@@ -1,9 +1,6 @@
 import React from 'react';
 
 var Info = (props) => {
-  // var keys = Object.keys(props.descObj).filter(key => key === props.currentText)
-  // console.log(keys, 'woo');
-// props.descObj[props.currentText] &&
   if (props.pinText === props.currentText && props.show) {
     return (
       <div className="notesText">
