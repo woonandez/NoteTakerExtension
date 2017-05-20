e9fffedd47d97f4c6a1c91b3fdd0f8e69d60e530
woonandez <matthew.allen.fernandez@gmail.com>
2017-05-19 20:25:01 -0700

    commit before merge

--- src/info.js
+++ src/info.js
@@ -3,9 +3,9 @@ import React from 'react';
 var Info = (props) => {
   if (props.descObj[props.pinText] && props.show) {
     return (
-      <h2>
-        {props.descObj[props.pinText]}
-      </h2>
+      <div className="notesText">
+        {props.descObj[props.currentText]}
+      </div>
     )
   } else {
     return (
