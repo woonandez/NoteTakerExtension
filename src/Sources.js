e9563610f21156297123d3e4c64baf2a58aacdd7
woonandez <matthew.allen.fernandez@gmail.com>
2017-05-21 16:36:01 -0700

    remove generic hover state from nested info items

--- src/Sources.js
+++ src/Sources.js
@@ -5,7 +5,7 @@ var Sources = (props) =>
     <div className="infoContainer">
       <div className="infoItem">
         <div className="infoTitle">
-          {props.title}
+          <a className="uniqueLink" href={props.link} target="_blank">{props.title}</a>
         </div>
         {props.sentence}
       </div>
